5d3f5770aedd500823004596ab71834ff75e043c
czarluc <czarluc@gmail.com>
2020-10-06 14:33:37 +0800

    change static

--- Procfile
+++ Procfile
@@ -1,1 +1,4 @@
+python manage.py collectstatic --noinput;
+
 web: gunicorn inventory.wsgi
+
--- inventory/settings.py
+++ inventory/settings.py
@@ -131,7 +131,9 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
