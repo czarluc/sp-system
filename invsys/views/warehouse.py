580c0941068db6a395753e0c9198cfbcf27e1a1a
czarluc <czarluc@gmail.com>
2020-10-18 16:37:03 +0800

    Fixed whse view

--- invsys/views/warehouse.py
+++ invsys/views/warehouse.py
@@ -3801,13 +3801,7 @@ def CheckWarehouse_getImage(request):
     bin_loc_image = ""
 
     for bin_loc in bin_loc_obj:
-        bin_loc_image = bin_loc.get("image")
-
-    if bin_loc_image == None:
-        bin_loc_image = "/media/noimage/noimage.png"
-    else:
-        for bin_loc in bin_loc_obj:
-            bin_loc_image = "/media/" + bin_loc.get("image")
+        bin_loc_image = "/media/" + bin_loc.get("image")
 
         
     data = {
